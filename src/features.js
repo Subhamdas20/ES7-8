d9ea0c4b0c2e48ccfe3ffea619ab06dd126a824a
Subhamdas20 <Subham21.sd@gmail.com>
2021-12-27 14:58:06 +0530

    Added : object values and entries

--- src/features.js
+++ src/features.js
@@ -1,3 +1,8 @@
-console.log(3**3)
- 
-console.log(3**5)
+const obj = {
+    name :"subham",
+    email:"subh@gmail.com",
+    ph : 7003321213
+}
+
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
